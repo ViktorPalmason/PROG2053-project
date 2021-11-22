124313b0aa1e86902f562c961fe9878590a7e29b
ViktorPalmason <viktorjp@stud.ntnu.no>
2021-11-22 15:08:31 +0100

    modified the project so we now use the web server to get data

--- projectSecondPart/components/userPhotos/userPhotos.jsx
+++ projectSecondPart/components/userPhotos/userPhotos.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import {
-  Typography,
-  
-} from '@material-ui/core';
-import './userPhotos.css';
+import { Typography } from "@material-ui/core";
+import "./userPhotos.css";
+import fetchModel from "../../lib/fetchModelData";
 
 /**
  * Define UserPhotos, a React componment of CS142 project #5
@@ -13,34 +11,64 @@ import './userPhotos.css';
 class UserPhotos extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      photoList: [],
+    };
 
+    if (window.location.href.toString().includes("/photos/")) {
+      var id = window.location.href.slice(47, 71);
+      var photoList = fetchModel("http://localhost:3000/photosOfUser/" + id);
+      photoList.then((response) => {
+        this.setState({ photoList: response.data });
+      });
+    }
   }
 
+  // contextChange() {
+  //   if (window.location.href.toString().includes("/photos/")) {
+  //     var id = window.location.href.slice(47, 71);
+  //     var photoList = fetchModel("http://localhost:3000/photosOfUser/" + id);
+  //     photoList.then((response) => {
+  //       this.setState({ photoList: response.data });
+  //     });
+  //   }
+  //   console.log(this.state.photoList);
+  // }
+
   render() {
-
-    var photos = window.cs142models.photoOfUserModel(this.props.match.params.userId);
-
     return (
-
       <div>
-      {photos.map((picture) => (
-        <div key={picture._id + picture.date_time}>
-          <img className="img" key={picture._id} src={'images/' + picture.file_name} width="40%"/>
-          <p key={picture.date_time} className="imgDate">Published: {picture.date_time}</p>
-    
-          {picture.comments ? picture.comments.map((test) => (
-            <div key={test.comment}>
-              <div className="box" ><Link to={"/users/" + test.user._id} className="link">{test.user.first_name} {test.user.last_name}</Link>
-              <p className="date">{test.date_time}</p> <p className="comment">{test.comment}</p> </div>
-              </div>
-          )) : <p className="nocom">Picture has no comments</p>}
-          </div>
-          
-      ))}
-      </div>
+        {this.state.photoList.map((picture) => (
+          <div key={picture._id + picture.date_time}>
+            <img
+              className="img"
+              key={picture._id}
+              src={"images/" + picture.file_name}
+              width="40%"
+            />
+            <p key={picture.date_time} className="imgDate">
+              Published: {picture.date_time}
+            </p>
 
+            {picture.comments ? (
+              picture.comments.map((test) => (
+                <div key={test.comment}>
+                  <div className="box">
+                    <Link to={"/users/" + test.user._id} className="link">
+                      {test.user.first_name} {test.user.last_name}
+                    </Link>
+                    <p className="date">{test.date_time}</p>{" "}
+                    <p className="comment">{test.comment}</p>{" "}
+                  </div>
+                </div>
+              ))
+            ) : (
+              <p className="nocom">Picture has no comments</p>
+            )}
+          </div>
+        ))}
+      </div>
     );
-      
   }
 }
 
--- projectSecondPart/lib/fetchModelData.js
+++ projectSecondPart/lib/fetchModelData.js
@@ -1,44 +1,41 @@
-
 var Promise = require("Promise");
 
 /**
-  * FetchModel - Fetch a model from the web server.
-  *     url - string - The URL to issue the GET request.
-  * Returns: a Promise that should be filled
-  * with the response of the GET request parsed
-  * as a JSON object and returned in the property
-  * named "data" of an object.
-  * If the requests has an error the promise should be
-  * rejected with an object contain the properties:
-  *    status:  The HTTP response status
-  *    statusText:  The statusText from the xhr request
-  *
-*/
+ * FetchModel - Fetch a model from the web server.
+ *     url - string - The URL to issue the GET request.
+ * Returns: a Promise that should be filled
+ * with the response of the GET request parsed
+ * as a JSON object and returned in the property
+ * named "data" of an object.
+ * If the requests has an error the promise should be
+ * rejected with an object contain the properties:
+ *    status:  The HTTP response status
+ *    statusText:  The statusText from the xhr request
+ *
+ */
 
 function fetchModel(url) {
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
 
-  return new Promise(function(resolve, reject) {
-      
-      var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (this.readyState != 4) {
+        return;
+      }
 
-      xhr.onreadystatechange = function() {
-        if (this.readyState != 4) {
-          return;
-        }
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve({ data: JSON.parse(xhr.responseText) });
+      } else {
+        setTimeout(
+          () => reject({ status: 501, statusText: xhr.statusText }),
+          0
+        );
+      }
+    };
 
-        if (xhr.status >= 200 && xhr.status < 300) {
-          resolve({data: JSON.parse(xhr.responseText)});
-        } else {
-          setTimeout(() => reject({status: 501,statusText: xhr.statusText}),0);
-        }
-
-      };
-
-      xhr.open("GET",url);
-      xhr.send();
-
+    xhr.open("GET", url);
+    xhr.send();
   });
-
 }
 
 export default fetchModel;
